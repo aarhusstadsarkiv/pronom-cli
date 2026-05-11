d40179390857a8b0d9cf1dd0e031a3305994bdaa
Simon G. <45692977+simonglob@users.noreply.github.com>
2026-05-11 10:12:36 +0200

    remove another redundant f-string

--- src/pronom_cli/repository/masterformats.py
+++ src/pronom_cli/repository/masterformats.py
@@ -43,7 +43,7 @@ class MasterFormatsRepository(Repository[MasterFormatEntry]):
                 Parsed YAML content when successful; ``None`` if the remote
                 request fails.
         """
-        cache_file = self.cache_dir / f"{filename}"
+        cache_file = self.cache_dir / filename
 
         if cache_file.exists():
             last_modified = datetime.fromtimestamp(cache_file.stat().st_mtime)
